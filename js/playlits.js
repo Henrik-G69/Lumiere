1c2eb26982eb20714987f8d7983042627aeee162
helton santos <heltonsanto001@gmail.com>
2025-05-10 19:08:05 -0300

    add tela de playlist

---
+++ js/playlits.js
@@ -0,0 +1,8 @@
+document.getElementById("AddPlaylist").addEventListener('click', ()=>{
+    document.getElementById("CreatePlaylist").style.display = 'flex'
+    
+})
+
+document.getElementById("Cancelar").addEventListener('click', ()=>{
+    document.getElementById("CreatePlaylist").style.display = 'none'
+})
